c4880e39ea2d97519559538fdceb0900d1db9e51
apurritano <apurritan@gmail.com>
2019-01-23 12:57:00 -0500

    First Commit
    Added all my seperate Maven Projects (Project 1 Questions)

---
+++ Project1_Question3/src/main/java/com/revature/Q3_Reducer.java
@@ -0,0 +1,27 @@
+package com.revature;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * 
+ * @author apurritano
+ * 
+ * This reducer method does not do much except for return the key value pairs of the selected data points.
+ *
+ */
+
+public class Q3_Reducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable>{
+
+	@Override
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+			throws IOException, InterruptedException{
+		
+		for (DoubleWritable value : values){
+			context.write(key, value);
+		}
+	}
+}
